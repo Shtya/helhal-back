397e539bc1e030c3722c044e8fdf5616ecc4979f
shtya <shtya54@gmail.com>
2025-12-19 19:14:16 +0200

    init

--- src/main.ts
+++ src/main.ts
@@ -8,35 +8,34 @@ import { ConfigService } from '@nestjs/config';
 import { QueryFailedErrorFilter } from 'common/QueryFailedErrorFilter';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const port = process.env.PORT || 3030;
-  // Get the ConfigService instance
-  const configService = app.get(ConfigService);
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
+	const port = process.env.PORT || 3030;
+	// Get the ConfigService instance
+	const configService = app.get(ConfigService);
 
-  app.useGlobalFilters(app.get(QueryFailedErrorFilter));
-  app.useStaticAssets(join(__dirname, '..', '..', '/uploads'), { prefix: '/uploads/' });
+	app.useGlobalFilters(app.get(QueryFailedErrorFilter));
+	app.useStaticAssets(join(__dirname, '..', '..', '/uploads'), { prefix: '/uploads/' });
 
-  app.enableCors({
-    // origin: configService.get('ALLOWED_ORIGINS')?.split(',') || '*',
-    origin: [
-      'https://helhal-front.vercel.app',
-      'https://main.d2ovvpcdqp4v8z.amplifyapp.com', // موقعك على Amplify
-      'http://localhost:3000', // للتجارب
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    allowedHeaders: 'Content-Type,Authorization,x-lang',
-    exposedHeaders: 'Content-Length,Content-Range',
-  });
+	app.enableCors({
+		// origin: configService.get('ALLOWED_ORIGINS')?.split(',') || '*',
+		origin: [
+			'https://www.helhal.com',
+			'http://localhost:3000', // للتجارب
+		],
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		credentials: true,
+		allowedHeaders: 'Content-Type,Authorization,x-lang',
+		exposedHeaders: 'Content-Length,Content-Range',
+	});
 
-  app.setGlobalPrefix('api/v1');
+	app.setGlobalPrefix('api/v1');
 
-  const loggingValidationPipe = app.get(LoggingValidationPipe);
-  app.useGlobalPipes(loggingValidationPipe);
+	const loggingValidationPipe = app.get(LoggingValidationPipe);
+	app.useGlobalPipes(loggingValidationPipe);
 
-  app.useGlobalPipes(new ValidationPipe({ disableErrorMessages: false, transform: true, forbidNonWhitelisted: true, whitelist: true }));
+	app.useGlobalPipes(new ValidationPipe({ disableErrorMessages: false, transform: true, forbidNonWhitelisted: true, whitelist: true }));
 
-  Logger.log(`🚀 server is running on port ${port}`);
-  await app.listen(port || 8081, '0.0.0.0');
+	Logger.log(`🚀 server is running on port ${port}`);
+	await app.listen(port || 8081, '0.0.0.0');
 }
 bootstrap();
